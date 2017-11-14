c575223fbf9fbe1d78c132ff51349ac9fdf242bf
Sowyn <m4goujon@enib.fr>
2017-11-14 14:28:00 +0100

    Lien bouton et nouvelle activité éclairage

--- app/src/main/java/manageyourhouse/myh_manageyourhouse/MainActivity.java
+++ app/src/main/java/manageyourhouse/myh_manageyourhouse/MainActivity.java
@@ -1,14 +1,25 @@
 package manageyourhouse.myh_manageyourhouse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        final Button ButtonEclairage = (Button) findViewById(R.id.buttonEclairage);
+        ButtonEclairage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Eclairage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
