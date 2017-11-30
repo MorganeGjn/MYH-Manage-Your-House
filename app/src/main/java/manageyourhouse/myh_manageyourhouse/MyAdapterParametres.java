5d763278d25837018c6fa517512e8c3e0238d79b
Sowyn <m4goujon@enib.fr>
2017-11-30 17:51:32 +0100

    Ajout champs de saisie

--- app/src/main/java/manageyourhouse/myh_manageyourhouse/MyAdapterParametres.java
+++ app/src/main/java/manageyourhouse/myh_manageyourhouse/MyAdapterParametres.java
@@ -3,22 +3,15 @@ package manageyourhouse.myh_manageyourhouse;
 import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
 import android.app.AlertDialog;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static manageyourhouse.myh_manageyourhouse.R.id.action_context_bar;
-import static manageyourhouse.myh_manageyourhouse.R.id.none;
-import static manageyourhouse.myh_manageyourhouse.R.layout.inputminutes;
 
 public class MyAdapterParametres extends RecyclerView.Adapter<MyAdapterParametres.MyViewHolder> {
 
@@ -62,48 +55,63 @@ public class MyAdapterParametres extends RecyclerView.Adapter<MyAdapterParametre
 
         public MyViewHolder(final View itemView) {
             super(itemView);
-            Pieces piece = Pieces.get(1);
-
             name = itemView.findViewById(R.id.name);
+            Pieces piece = Pieces.get(1);
             for (int i = 0; i <Pieces.size(); i++){
                 if (currentPair == Pieces.get(i).getName()){
                     piece = Pieces.get(i);
                 }
             }
-            final boolean[] Notif = new boolean[]{piece.getNotifiation()};
-            final CharSequence[] items = {"Notification"};
             final ArrayList seletedItems = new ArrayList();
-
             itemView.setOnClickListener(new View.OnClickListener() {
+
                 @Override
                 public void onClick(View view) {
-                   AlertDialog dialog = new AlertDialog.Builder(itemView.getContext())
-                            .setTitle(currentPair)
-                            .setMultiChoiceItems(items, Notif, new DialogInterface.OnMultiChoiceClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int indexSelected, boolean isChecked) {
-                                    if (isChecked) {
-                                        // If the user checked the item, add it to the selected items
-                                        seletedItems.add(indexSelected);
-                                    }
-                                    else if (seletedItems.contains(indexSelected)) {
-                                        // Else, if the item is already in the array, remove it
-                                        seletedItems.remove(Integer.valueOf(indexSelected));
-                                    }
+                    Pieces piece = Pieces.get(1);
+                    for (int i = 0; i <Pieces.size(); i++){
+                        if (currentPair == Pieces.get(i).getName()){
+                            piece = Pieces.get(i);
+                        }
+                    }
+                    final boolean[] Notif = new boolean[]{piece.getNotifiation()};
+                    final CharSequence[] items = {"Notification"};
+                    final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(itemView.getContext());
+                    LayoutInflater factory = LayoutInflater.from(itemView.getContext());
+                    final View dialogView = factory.inflate(R.layout.alertdialog, null);
+                    final EditText edit=(EditText) dialogView.findViewById(R.id.minutes);
+                    String min = String.valueOf(piece.getMinutes());
+                    edit.setText(min);
+                    dialogBuilder.setView(dialogView);
+                    dialogBuilder.setTitle(currentPair);
+                    dialogBuilder.setMultiChoiceItems(items, Notif, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int indexSelected, boolean isChecked) {
+                            if (isChecked) {
+                                // If the user checked the item, add it to the selected items
+                                seletedItems.add(indexSelected);
+                            }
+                            else if (seletedItems.contains(indexSelected)) {
+                                // Else, if the item is already in the array, remove it
+                                seletedItems.remove(Integer.valueOf(indexSelected));
+                            }
+                        }
+                    });
+                    dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            Pieces piece = Pieces.get(1);
+                            for (int i = 0; i <Pieces.size(); i++){
+                                if (currentPair == Pieces.get(i).getName()){
+                                    piece = Pieces.get(i);
                                 }
-                            })
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int id) {
-                                            //  Your code when user clicked on OK
-                                            //  You can write the code  to save the selected item here
-                                        }
-                                    })
-                            .create();
-                    final EditText min = (EditText)itemView.findViewById(R.id.minutes);
+                            }
+                            String text = edit.getText().toString();
+                            piece.setMinutes(Integer.parseInt(text));
+                        }
+                    });
+                    AlertDialog b = dialogBuilder.create();
+                    b.show();
 
-                    dialog.setView(min);
-                    dialog.show();
                 }
             });
             if (seletedItems.size() != 0) {
